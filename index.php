a4ea0c2895f8721df53eab9c94793ced53d65dfe
PepeCoca <76112909+PepeCoca@users.noreply.github.com>
2020-12-22 19:14:18 +0500

    21.12.2020_zd3

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
-<?
+<?php
+$time = date('H');
+if($time<7)
+{
+    $img = '01.jpg';
+}
+elseif($time<7 && $time>=11 )
+{
+    $img = '02.jpg';
+}
+elseif($time>=11 && $time<16)
+{
+    $img = '03.jpg';
+}
+elseif($time>=16 && $time<=20)
+{
+    $img = '04.jpg';
+}
 
-$a=10;
-$b=5;
-define("TT", $a+$b);
-echo TT;
 
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div> <?php
+echo $time;
+echo '<img src="'. $img .'">'; ?>
+</div>
+
+
+</body>
+</html>
 
 
