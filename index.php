cd486e9fdc473c7b0bbb9b2b65a5af7d217818a6
PepeCoca <76112909+PepeCoca@users.noreply.github.com>
2020-12-27 15:21:03 +0500

    25.12.2020

--- index.php
+++ index.php
@@ -1,48 +1,49 @@
 <?
 
 //Задание 1
-for($i=1;$i<10;$i++)
-{
-    $ar[]=rand(1,10);
+$str = 'Takes stake a back';
+$len = strlen($str);
+if ($len > 5) {
+    echo substr($str, 0, 5) . '...';
+} else {
+    echo $str;
 
-    if(($ar[$i] % 2) == 0)
-    {
-        echo "<b>$ar[$i]</b><br/>";
-    }
-    else
-        {
-        echo "<span>$ar[$i]</span><br/>";
-    }
-    $ar[$i] ++ ;
 }
 echo'<br>';
 
 //Задание 2
-$arr11 = array( array("розы", "апельсин" , "Ананас"),
-    array("тюльпаны", "арбуз" , "ром"),
-    array("орхидеи", "гречка" )
-);
-foreach ($arr11 as $a)
-{
-    foreach ($a as $s)
-    {
-        mb_substr($s, 0, 1) != 'а' ?: print "$s<br>";
-        mb_substr($s, 0, 1) != 'А' ?: print "$s<br>";
-    }
+$str = 'abc';
+echo str_replace(['a', 'b', 'c'], [1, 2, 3], 'abc');
 
-}
 echo'<br>';
 
 //Задание 3
-$arr12 = array(
-    'Однозначные'=>array(5, 6 ),
-    'Двухзначные'=>array(50, 60 , 70),
-    'Трёхзначные'=>array(500,600, 700 ,800,400)
-);
-echo count($arr12['Однозначные']).'<br>';
-echo count($arr12['Двухзначные']).'<br>';
-echo count($arr12['Трёхзначные']).'<br>';
-echo count($arr12, COUNT_RECURSIVE).'<br>';
+$str = 'abc abc abc';
+echo strrpos($str, 'c');
+echo '<br>';
+
+//Задание 4
+$str = 'html css php';
+$arr = explode(' ', $str);
+foreach ($arr as $i){
+    echo $i .'<br>';
+}
+
+//Задание 5
+$date1 = date_create("20-01-2020");
+$date2 = date_create("20-02-2021");
+$arr1 = date_diff($date1,$date2);
+echo "Количество дней : ". $arr1->days.'<br>';
+
+//Задание 5.1
+$str ='pink.png';
+if (substr($str, -4,4) == '.png') {
+    echo 'да';
+} else {
+    echo 'нет';
+}
+
+
 
 
 
